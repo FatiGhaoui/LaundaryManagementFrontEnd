b50af1450a34efba4a86aad14ad333ba0405b398
AXimenaGaleano <145374738+AXimenaGaleano@users.noreply.github.com>
2024-01-12 00:15:47 +0100

    Update backend.config.js Connection to clevercloud

--- backend.config.js
+++ backend.config.js
@@ -1,2 +1,2 @@
-export const backendHost = 'http://localhost:8080';
-//export const backendHost = 'https://laundryapp.cleverapps.io';
+//export const backendHost = 'http://localhost:8080';
+export const backendHost = 'https://laundryapp.cleverapps.io';
